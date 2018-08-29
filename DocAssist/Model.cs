3efd64ad3e7fc880d8485c068574a02501c1e19d
SiddheshwarKore <siddheshwarskore@gmail.com>
2018-08-29 20:17:27 +0530

    Update Model.cs

--- DocAssist/Model.cs
+++ DocAssist/Model.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 namespace DocAssist
-{
+{   //Model Class programmatically represents each physical module in UiPath project with 
+    //attributes such as filepath, list of Datatables contaning module level details of variables,arguments etc and a single datatable contatining application used and web services details
     class Model
     {
         public string XamlPath { get; set; }
